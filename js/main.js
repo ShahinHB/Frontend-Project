fbfa12f20f839fdc69e8fdb047ad6e3d99548f40
ShahinHB <72246220+ShahinHB@users.noreply.github.com>
2021-02-04 13:29:34 +0400

    Some Changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    dots:true,
     responsive:{
         0:{
             items:1
